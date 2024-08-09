166bdbc6f0c6b75700867abb7e498ddccdad5901
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2024-08-09 21:21:01 +0200

    Open social links in a new tab

--- src/components/SocialNetwork.jsx
+++ src/components/SocialNetwork.jsx
@@ -5,9 +5,9 @@ import codepenIcon from "../assets/images/icons/codepen.svg"
 const SocialNetwork = () => {
     return (
         <div className="social-network">
-            <a href="https://www.linkedin.com/in/steve-basse/" alt="linkedin" title="LinkedIn"><img src={linkedinIcon} alt="linkedin-icon"/></a>
-            <a href="https://github.com/iSteveB" alt="github" title="GitHub"><img src={githubIcon} alt="github-icon"/></a>
-            <a href="https://codepen.io/isteveB" alt="codePen" title="CodePen"><img src={codepenIcon} alt="codepen-icon"/></a>
+            <a href="https://www.linkedin.com/in/steve-basse/" target="_blank" rel='noreferrer' alt="linkedin" title="LinkedIn"><img src={linkedinIcon} alt="linkedin-icon"/></a>
+            <a href="https://github.com/iSteveB" target="_blank" rel='noreferrer' alt="github" title="GitHub"><img src={githubIcon} alt="github-icon"/></a>
+            <a href="https://codepen.io/isteveB" target="_blank" rel='noreferrer' alt="codePen" title="CodePen"><img src={codepenIcon} alt="codepen-icon"/></a>
         </div>
     );
 };
