1cef6411583b8e0370709dd48a5401f5b1132d4c
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2024-08-09 21:11:57 +0200

    Use miniature of images

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable react/prop-types */
 import eye from '../../assets/images/icons/eye.svg';
 import githubIcon from '../../assets/images/icons/githubIcon.svg';
+import { getProjectImage } from '../../utils/getProjectImage';
 
 const Card = ({work}) => {
+  const { thumbnail } = getProjectImage(work.image);
   return (
     <div className='work-card'>
-      <img className="work-card__image" src={work.image} alt={work.name} />
+      <img className="work-card__image" src={thumbnail} alt={work.name} />
       <div className="work-card__links">
         {work.link && <a href={work.link} target="_blank" rel="noreferrer"><img src={eye} alt="Voir en live" title='Voir en live'/></a>}
         <a href={work.github} target="_blank" rel="noreferrer"><img src={githubIcon} alt="Voir sur GitHub" title='Voir sur GitHub' /></a>
