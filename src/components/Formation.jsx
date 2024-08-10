06886015fedcf8505612da08abaf8b5ae9802946
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2024-08-11 00:04:13 +0200

    feat (ui) : create formation section

---
+++ src/components/Formation.jsx
@@ -0,0 +1,25 @@
+import { motion } from 'framer-motion';
+
+const Formation = () => {
+    return (
+        <motion.div 
+            className="formation"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+        >
+            <h3>Développeur / Intégrateur Web</h3>
+            <p>Titre RNCP 5 - @OpenClassrooms</p>
+            <span>Mai 2024 - Août 2024</span>
+            <ul>
+                <li>Conception et développement d&apos;interfaces utilisateurs avec <strong>React</strong>.</li>
+                <li><strong>Collaboration</strong> avec des designers pour traduire des maquettes en <strong>applications fonctionnelles</strong>.</li>
+                <li><strong>Intégration de données</strong> provenant de diverses sources back-end et bases de données.</li>
+                <li><strong>Optimisation d&apos;applications</strong> pour maximiser leur vitesse, leur efficacité et le <strong>SEO</strong>. </li>
+                <li> <strong>Gestion de projet </strong> web grâce à la <strong>méthode agile</strong>.</li>
+            </ul>
+        </motion.div>
+    );
+};
+
+export default Formation;
--- src/pages/Achievements.jsx
+++ src/pages/Achievements.jsx
@@ -2,17 +2,42 @@ import { motion } from 'framer-motion';
 import Header from '../components/layout/Header';
 import Works from '../components/layout/Works';
 import TechStack from '../components/TechStack';
+import Formation from '../components/Formation';
 
 const Achievements = () => {
 	return (
 		<motion.div className='achievements'>
 			<Header />
-			<h1>Mon Parcours Tech</h1>
-			<h2>Stack technique</h2>
-			<TechStack />
-			<h2>Formation</h2>
-			<h2>Mes Projets</h2>
-			<Works />
+			<motion.h1
+				initial={{ opacity: 0, y: -20 }}
+				animate={{ opacity: 1, y: 0 }}
+				transition={{ duration: 0.5 }}>
+				Mon Parcours Tech
+			</motion.h1>
+
+			<motion.section
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				transition={{ delay: 0.2, duration: 0.5 }}>
+				<h2>Stack technique</h2>
+				<TechStack />
+			</motion.section>
+
+			<motion.section
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				transition={{ delay: 0.4, duration: 0.5 }}>
+				<h2>Formation</h2>
+				<Formation />
+			</motion.section>
+
+			<motion.section
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				transition={{ delay: 0.6, duration: 0.5 }}>
+				<h2>Mes Projets</h2>
+				<Works />
+			</motion.section>
 		</motion.div>
 	);
 };
